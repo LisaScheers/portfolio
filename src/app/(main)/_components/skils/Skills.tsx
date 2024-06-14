ec5c875075f54a22e43ea7dc5547ebb296cc350a
Lisa Scheers <lisa@scheers.tech>
2024-06-14 03:32:51 +0200

    fix allignment

--- src/app/(main)/_components/skils/Skills.tsx
+++ src/app/(main)/_components/skils/Skills.tsx
@@ -17,7 +17,7 @@ export default function Skills() {
             </p>
           </div>
         </div>
-        <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3 lg:gap-12">
+        <div className="mx-auto grid max-w-5xl gap-6 py-12 align-top lg:grid-cols-3 lg:gap-12">
           <Suspense fallback={<SkillSuspense />}>
             <SkillList />
           </Suspense>
